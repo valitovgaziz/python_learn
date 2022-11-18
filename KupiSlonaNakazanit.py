4883e89cf046b20a9b07ff74287aaff16a12a494
ValitovGaziz <valitovgaziz@gmail.com>
2022-11-18 20:05:34 +0500

    kupi slona

---
+++ KupiSlonaNakazanit.py
@@ -0,0 +1,4 @@
+number = int(input())
+nakaz = input()
+for num in range(0, number):
+    print(f'Я больше никогда не буду писать "{nakaz}"!')
---
+++ bayElephant.py
@@ -0,0 +1,3 @@
+number = int(input())
+for num in range(0, number):
+    print('Купи слона!')
