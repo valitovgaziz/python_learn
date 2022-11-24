5ea1e5e046701d21665a3ceaa946158487edf867
gazizkv <valitovgaziz@gmail.com>
2022-11-24 19:05:14 +0500

    Who faster

---
+++ ConditionalOperators/SkiRace.py
@@ -0,0 +1,7 @@
+Pety = int(input())
+Vasy = int(input())
+if Pety > Vasy:
+    result = 'Петя'
+else:
+    result = 'Вася'
+print(result)
---
+++ ConditionalOperators/WhoFaster.py
@@ -0,0 +1,10 @@
+Pety = int(input())
+Vasy = int(input())
+Toly = int(input())
+
+if Pety >= Vasy and Pety >= Toly:
+    print('Петя')
+elif Vasy >= Pety and Vasy >= Toly:
+    print('Вася')
+elif Toly >= Pety and Toly >= Vasy:
+    print('Толя')
