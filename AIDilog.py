37d95ecbb99755d6aee03713ee13e7d8051bd158
gazizkv <valitovgaziz@gmail.com>
2022-11-24 16:30:40 +0500

    Simple Ai dialog

---
+++ AIDilog.py
@@ -0,0 +1,7 @@
+name = input('Как Вас зовут?\n')
+print(f'Здравствуйте, {name}!')
+howAreYou = input('Как дела?\n')
+if howAreYou == 'хорошо':
+    print(f'Я за вас рада!')
+else:
+    print('Всё наладится!')
