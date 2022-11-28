bbf082c0953659e5d617567765ed6a0d9b4db587
gazizkv <valitovgaziz@gmail.com>
2022-11-28 06:44:01 +0500

    Leap year or not.

---
+++ ConditionalOperators/LeapYear.py
@@ -0,0 +1,12 @@
+year = int(input())
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("YES")
+        else:
+            print("NO")
+    else:
+        print("YES")
+else:
+    print("NO")
