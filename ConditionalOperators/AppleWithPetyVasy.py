cd6eb891d77cd6fd3478d5a938339b658c7f7aa4
gazizkv <valitovgaziz@gmail.com>
2022-11-28 05:40:52 +0500

    как делили яблоки Вася и Петя

---
+++ ConditionalOperators/AppleWithPetyVasy.py
@@ -0,0 +1,11 @@
+Pety = 7
+Vasy = 6
+Pety = Pety - 3 + 2
+Vasy = Vasy + 3 + 5 - 2
+Pety = Pety + int(input())
+Vasy = Vasy + int(input())
+
+if Pety > Vasy:
+    print('Петя')
+else:
+    print('Вася')
