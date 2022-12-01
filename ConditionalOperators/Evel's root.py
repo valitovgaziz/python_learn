bb9cbe7349e58e9654cf34a997345d7262aa2413
valitovgaziz <valitovgaziz@gmail.com>
2022-12-01 20:36:21 +0500

    Evel's root.py not find solution bit fube eat

--- ConditionalOperators/Evel's root.py
+++ ConditionalOperators/Evel's root.py
@@ -3,7 +3,7 @@ import math
 a = float(input())
 b = float(input())
 c = float(input())
-if a == 0 and b == 0:
+if a == 0 and b == 0 and c == 0:
     print('Infinite solutions')
     exit()
 D = math.pow(b, 2) - (4 * a * c)
