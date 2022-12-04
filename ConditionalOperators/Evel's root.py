11b40121b4992ba0fcc7b28730a6543198e19e22
valitovgaziz <valitovgaziz@gmail.com>
2022-12-04 08:38:17 +0500

    Not found solution on evil's root.

--- ConditionalOperators/Evel's root.py
+++ ConditionalOperators/Evel's root.py
@@ -6,15 +6,21 @@ c = float(input())
 if a == 0 and b == 0 and c == 0:
     print('Infinite solutions')
     exit()
+elif a== 0 and b == 0 and c != 0:
+    print('No solution')
+    exit()
 D = math.pow(b, 2) - (4 * a * c)
 if D > 0:
-    x1 = ((-b) - math.sqrt(D)) / (2 * a)
-    x2 = ((-b) + math.sqrt(D)) / (2 * a)
+    x1 = round(((-b) - math.sqrt(D)) / (2 * a), 2)
+    x2 = round(((-b) + math.sqrt(D)) / (2 * a), 2)
     x1 = '{:.2f}'.format(x1)
     x2 = '{:.2f}'.format(x2)
-    print(f'{x1} {x2}')
+    if x1 > x2:
+        print(f'{x1} {x2}')
+    else:
+        print(f'{x2} {x1}')
 elif D == 0:
-    x = -b / (2 * a)
+    x = round(-b / (2 * a), 2)
     x = '{:.2f}'.format(x)
     print(str(x))
 else:
